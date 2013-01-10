e25e62bf03c913442ac5687fe5021875752a38c4
Yuji Akiyama <kjprh366@ybb.ne.jp>
2013-01-10 13:12:45 +0900

    dentaku

--- dentaku/dentakuViewController.h
+++ dentaku/dentakuViewController.h
@@ -9,5 +9,45 @@
 #import <UIKit/UIKit.h>
 
 @interface dentakuViewController : UIViewController
+{
+
+    double a;
+    double b;
+    double x,y;
+    double A;
+    
+    
+    int shisoku;
+    int enzan;
+}
+
+
+
+@property enum {Normal,Dot} hyouji;
+@property enum {Add,Sub,Mult,Divi,Equal} keisan;
+
+
+- (IBAction)key0:(id)sender;
+- (IBAction)key1:(id)sender;
+- (IBAction)key2:(id)sender;
+- (IBAction)key3:(id)sender;
+- (IBAction)key4:(id)sender;
+- (IBAction)key5:(id)sender;
+- (IBAction)key6:(id)sender;
+- (IBAction)key7:(id)sender;
+- (IBAction)key8:(id)sender;
+- (IBAction)key9:(id)sender;
+- (IBAction)add:(id)sender;
+- (IBAction)sub:(id)sender;
+- (IBAction)mult:(id)sender;
+- (IBAction)divi:(id)sender;
+- (IBAction)equal:(id)sender;
+- (IBAction)allclear:(id)sender;
+- (IBAction)period:(id)sender;
+- (IBAction)key00:(id)sender;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *display;
 
 @end
+
--- dentaku/dentakuViewController.m
+++ dentaku/dentakuViewController.m
@@ -14,10 +14,12 @@
 
 @implementation dentakuViewController
 
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+   	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +28,201 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)key0:(id)sender {
+    b=0;
+    a=a*10+b;
+    [[self display] setText:[NSString stringWithFormat:@"%.0f", a]];
+}
+
+- (IBAction)key1:(id)sender {
+    b=1;
+    a=a*10+b;
+    [[self display] setText:[NSString stringWithFormat:@"%.0f", a]];
+}
+
+- (IBAction)key2:(id)sender {
+    b=2;
+    a=a*10+b;
+    [[self display] setText:[NSString stringWithFormat:@"%.0f", a]];
+}
+
+- (IBAction)key3:(id)sender {
+    b=3;
+    a=a*10+b;
+    [[self display] setText:[NSString stringWithFormat:@"%.0f", a]];
+}
+
+- (IBAction)key4:(id)sender {
+    b=4;
+    a=a*10+b;
+    [[self display] setText:[NSString stringWithFormat:@"%.0f", a]];
+}
+
+- (IBAction)key5:(id)sender {
+    b=5;
+    a=a*10+b;
+    [[self display] setText:[NSString stringWithFormat:@"%.0f", a]];
+}
+
+- (IBAction)key6:(id)sender {
+    b=6;
+    a=a*10+b;
+    [[self display] setText:[NSString stringWithFormat:@"%.0f", a]];
+}
+
+- (IBAction)key7:(id)sender {
+    b=7;
+    a=a*10+b;
+    [[self display] setText:[NSString stringWithFormat:@"%.0f", a]];
+}
+
+- (IBAction)key8:(id)sender {
+    b=8;
+    a=a*10+b;
+    [[self display] setText:[NSString stringWithFormat:@"%.0f", a]];
+}
+
+- (IBAction)key9:(id)sender {
+    b=9;
+    a=a*10+b;
+    [[self display] setText:[NSString stringWithFormat:@"%.0f", a]];
+}
+- (IBAction)key00:(id)sender {
+    b=00;
+    a=a*100+b;
+    [[self display] setText:[NSString stringWithFormat:@"%.0f",a]];
+}
+
+
+- (IBAction)allclear:(id)sender {
+    a=0;
+    x=0;
+    
+    [[self display] setText:[NSString stringWithFormat:@"%.0f",a]];
+}
+
+
+
+- (IBAction)equal:(id)sender{
+    switch([self keisan]) {
+        case Equal:
+            x=a;
+            break;
+        case Add:
+            x=x+a;
+            break;
+        case Sub:
+            x=x-a;
+            break;
+        case Mult:
+            x=x*a;
+            break;
+        case Divi:
+            x=x/a;
+            break;
+    }
+    
+    [[self display] setText:[NSString stringWithFormat:@"%.0f", x]];
+}
+
+
+- (IBAction)add:(id)sender {
+    switch([self keisan]) {
+        case Equal:
+            x=a;
+            break;
+        case Add:
+            x=x+a;
+            break;
+        case Sub:
+            x=x-a;
+            break;
+        case Mult:
+            x=x*a;
+            break;
+        case Divi:
+            x=x/a;
+            break;
+    }
+    
+    [self setKeisan:Add];
+    a=0;
+}
+
+- (IBAction)sub:(id)sender {
+    switch([self keisan]) {
+        case Equal:
+            x=a;
+        break;
+        case Add:
+            x=x+a;
+            break;
+        case Sub:
+            x=x-a;
+            break;
+        case Mult:
+            x=x*a;
+            break;
+        case Divi:
+            x=x/a;
+            break;
+    }
+
+    [self setKeisan:Sub];
+    a=0;
+    
+
+}
+
+- (IBAction)mult:(id)sender {
+    switch([self keisan]) {
+        case Equal:
+            x=a;
+            break;
+        case Add:
+            x=x+a;
+            break;
+        case Sub:
+            x=x-a;
+            break;
+        case Mult:
+            x=x*a;
+            break;
+        case Divi:
+            x=x/a;
+            break;
+    }
+    [self setKeisan:Mult];
+    a=0;
+}
+
+- (IBAction)divi:(id)sender {
+    switch([self keisan]) {
+        case Equal:
+            x=a;
+            break;
+        case Add:
+            x=x+a;
+            break;
+        case Sub:
+            x=x-a;
+            break;
+        case Mult:
+            x=x*a;
+            break;
+        case Divi:
+            x=x/a;
+            break;
+    }
+    [self setKeisan:Divi];
+    a=0;
+
+}
+
+
+- (IBAction)period:(id)sender {
+    
+}
+     
 @end
